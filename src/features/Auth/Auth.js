0056cf8d748d174e24137c531c6338be3a3f9946
newswim <danminshew@gmail.com>
2017-11-30 20:56:39 -0600

    Update clientID

--- src/features/Auth/Auth.js
+++ src/features/Auth/Auth.js
@@ -4,7 +4,7 @@ import auth0 from 'auth0-js'
 export default class Auth {
   auth0 = new auth0.WebAuth({
     domain: 'abs-test.auth0.com',
-    clientID: 'O2PGm9flUvIuS3fpr1BFDlKrD1SMSUzu',
+    clientID: 'GNXNW4dcw0C10doA6AX1yPXERiN4flev',
     redirectUri: 'http://localhost:3000/callback',
     audience: 'https://abs-test.auth0.com/userinfo',
     responseType: 'token id_token',
